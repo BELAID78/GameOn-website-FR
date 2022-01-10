5f1a3238318e6dc5d209347ac44ba1accc3b03ee
BELAID78 <92091235+BELAID78@users.noreply.github.com>
2022-01-10 17:59:54 +0100

    launchThank & launch bouton js edit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,95 +1,106 @@
 // Nav responsive
 function editNav() {
-  var nav = document.getElementById("myTopnav");
-  if (nav.className === "topnav") {
-    nav.className += " responsive";
-  } else {
-    nav.className = "topnav";
+	var nav = document.getElementById("myTopnav");
+	if (nav.className === "topnav") {
+	  nav.className += " responsive";
+	} else {
+	  nav.className = "topnav";
+	}
   }
-}
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-
-// Clic "je m'inscris"
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// Lancement formulaire (changement du display en block)
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-// Function Message erreur
-function showErrorMessage(champForm, formRempli, message) {
-	champForm.target.setCustomValidity("");
-	if (!champForm.target.validity.valid && champForm.target.value.length == 0) {
-			champForm.target.setCustomValidity(message);
-	}
-};
-
-// Messages d'erreurs par champs pour le formulaire
-document.getElementById("inscription").addEventListener("click", function(formRempli) {
-
-	let firstName = document.getElementById("first");
-	let lastName = document.getElementById("last");
-	let emailValid = document.getElementById("email");
-	let birthValid = document.getElementById("birthdate");
-	let quantityCity = document.getElementById("quantity");
-	let checkBox = document.getElementById("checkbox1");
-
-	const thankBtn = document.querySelectorAll(".thank-btn");
-	const thankBg = document.querySelector(".bground-thank");
-	
-	if (!firstName.value) {
-		firstName.oninvalid = function(champForm) {
-			showErrorMessage(champForm, formRempli, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
-		};	
-		return false;	
-	}
-	if (!lastName.value) {
-		lastName.oninvalid = function(champForm) {
-			showErrorMessage(champForm, formRempli, "Veuillez entrer 2 caractères ou plus pour le champ du nom.")
-		};
-		return false;	
-	} 
-	if (!emailValid.value) {
-		emailValid.oninvalid = function(champForm) {
-			showErrorMessage(champForm, formRempli, "Entrez une adresse valide. Exemple : contact@nom.com")
-		};
-		return false;	
-	}
-	if (!birthValid.value) {
-		birthValid.oninvalid = function(champForm) {
-			showErrorMessage(champForm, formRempli, "Vous devez entrer votre date de naissance.")
-		};
-		return false;	
-	}
-	if (!quantityCity.value) {
-		quantityCity.oninvalid = function(champForm) {
-			showErrorMessage(champForm, formRempli, "Vous devez renseigner ce champ.")
-		};
-		return false;	
-	}
-	if (!checkBox.value) {
-		checkBox.oninvalid = function(champForm, formRempli, message) {
-				champForm.target.setCustomValidity("");
-				if (!champForm.checked) {
-						return true;
-				}
-				else {
-					champForm.target.setCustomValidity("Vous devez renseigner ce champ.");
-				}
-			};
-		return false;	
-	}
-	else {
-		thankBtn.forEach((btn) => btn.addEventListener("click", launchThank));
-		function launchThank() {
-			thankBg.style.display = "block";
+  
+  // DOM Elements
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const formData = document.querySelectorAll(".formData");
+  const thankBtn = document.querySelector(".thank-btn");
+  
+  
+  // Clic "je m'inscris"
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  
+  // Lancement formulaire (changement du display en block)
+  function launchModal() {
+	modalbg.style.display = "block";
+  }
+  
+  
+  
+  // Function Message erreur
+  function showErrorMessage(champForm, formRempli, message) {
+	  champForm.target.setCustomValidity("");
+	  if (!champForm.target.validity.valid && champForm.target.value.length == 0) {
+			  champForm.target.setCustomValidity(message);
+	  }
+  };
+  
+  // Messages d'erreurs par champs pour le formulaire
+  document.getElementById("inscription").addEventListener("click", function(formRempli) {
+  
+	  let firstName = document.getElementById("first");
+	  let lastName = document.getElementById("last");
+	  let emailValid = document.getElementById("email");
+	  let birthValid = document.getElementById("birthdate");
+	  let quantityCity = document.getElementById("quantity");
+	  let checkBox = document.getElementById("checkbox1");
+  
+	  const thankBtn = document.querySelectorAll(".thank-btn");
+	  const thankBg = document.querySelector(".bground-thank");
+	  
+	  if (!firstName.value) {
+		  firstName.oninvalid = function(champForm) {
+			  showErrorMessage(champForm, formRempli, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
+		  };	
+		  return false;	
+	  }
+	  if (!lastName.value) {
+		  lastName.oninvalid = function(champForm) {
+			  showErrorMessage(champForm, formRempli, "Veuillez entrer 2 caractères ou plus pour le champ du nom.")
+		  };
+		  return false;	
+	  } 
+	  if (!emailValid.value) {
+		  emailValid.oninvalid = function(champForm) {
+			  showErrorMessage(champForm, formRempli, "Entrez une adresse valide. Exemple : contact@nom.com")
+		  };
+		  return false;	
+	  }
+	  if (!birthValid.value) {
+		  birthValid.oninvalid = function(champForm) {
+			  showErrorMessage(champForm, formRempli, "Vous devez entrer votre date de naissance.")
+		  };
+		  return false;	
+	  }
+	  if (!quantityCity.value) {
+		  quantityCity.oninvalid = function(champForm) {
+			  showErrorMessage(champForm, formRempli, "Vous devez renseigner ce champ.")
+		  };
+		  return false;	
+	  }
+	  else {
+		  thankBtn.forEach((btn) => btn.addEventListener("click", launchThank));
+		  function launchThank() {
+			  thankBg.style.display = "block";
+			}
 		  }
-		}
-	}
-	);
+	  }
+	  );
+  
+  
+  // Lancement "Merci de votre participation"
+  thankBtn.forEach((btn) => btn.addEventListener("click", launchThank));
+  function launchThank() {
+	thankBg.style.display = "block";
+  }
+  
+  /* 	if (!checkBox.value) {
+		  checkBox.oninvalid = function(champForm, formRempli, message) {
+				  champForm.target.setCustomValidity("");
+				  if (!champForm.checked) {
+						  return true;
+				  }
+				  else {
+					  champForm.target.setCustomValidity("Vous devez renseigner ce champ.");
+				  }
+			  };
+		  return false;	
+	  } */
